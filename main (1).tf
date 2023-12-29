bc6e0c09d72b45d1ff4273bc2885bd322c932699
praveenpk2704 <141400620+praveenpk2704@users.noreply.github.com>
2023-12-29 14:28:33 +0530

    Add files via upload

---
+++ main (1).tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "ap-south-1"
+
+}
+
+resource "aws_instance" "key" {
+ami = "ami-012b9156f755804f5"
+instance_type = "t2.micro"
+key_name = "nwkpp"
+count = 1
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+tags = {
+Name = "project-instance"
+Environment = "dev"
+}
+}
